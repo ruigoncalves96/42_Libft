dea56a4605ae34091f41f4b44d49dfb141e27e32
Rui Gonçalves <ruigoncalves@Ruis-MBP.lan>
2024-04-13 00:40:42 +0100

    13/04 00:40

---
+++ ft_strncmp.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/13 00:05:32 by randrade          #+#    #+#             */
+/*   Updated: 2024/04/13 00:38:45 by randrade         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	if (s1 == NULL || s2 == NULL || !n)
+		return (0);
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
+		i++;
+	return (s1[i] - s2[i]);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char	*str1 = "Hello World!";
+	char	*str2 = "Hello ";
+	int	x = 12;
+
+	printf("%d\n", ft_strncmp(str2, str1, x));
+}*/
