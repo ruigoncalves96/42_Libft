682dea5688fc3ed6e291e85ead33ef1e91b9cc5c
Rui Gonçalves <ruigoncalves@Ruis-MBP.lan>
2024-04-12 20:09:31 +0100

    12/04 20:09

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -11,19 +11,41 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-/*
+
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t	src_len;
+
+	src_len = ft_strlen(src);
+	if (dst == NULL || src == NULL || !size)
+		return (src_len);
+	while (*src && --size)
+		*dst++ = *src++;
+	*dst = '\0';
+	return (src_len);
 }
-*/
-#include <stdio.h>
+/*
 #include <string.h>
+#include <stddef.h>
+#include <stdio.h>
 
 int	main(void)
 {
-	char	src[] = "Hello World!";
+	char	*str = "HELLO WORLD!";
 	char	dest[15];
+	size_t	size = 13;
+	size_t	x;
 
-	strlcpy(dest, src, 5);
-	printf("%s\n", dest);
-}
+	printf("String src before function: %s\n", str);
+	printf("String dest before function: %s\n", dest);
+	
+	x = ft_strlcpy(dest, str, size);
+
+	printf("String src: %s\n", str);
+	printf("String dest: %s\n", dest);
+	if (x)
+		printf("String dest last byte character: %d\n", dest[size - 1]);
+	printf("%zu\n", x);
+
+	return (0);
+}*/
