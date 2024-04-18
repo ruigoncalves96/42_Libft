dbbc6210cca78043f47327dce7a3e2f92a467bff
Rui Alexandre Andrade Goncalves <randrade@c1r9s1.42lisboa.com>
2024-04-18 17:08:27 +0100

    18/04 17:08

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/18 15:58:46 by randrade          #+#    #+#             */
-/*   Updated: 2024/04/18 16:45:29 by randrade         ###   ########.fr       */
+/*   Updated: 2024/04/18 17:03:33 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	ft_strlcpy(ptr, s+start, len + 1);
 	return (ptr);
 }
-/*
+
 #include <stdio.h>
 
 int	main(void)
 {
-	char	str[] = "Hello World!";
-	char	*str2 = ft_substr(str, 3, 5);
+	char	str[] = "Hello World! My name is Rui.";
+	char	*str2 = ft_substr("tripouille", 0, 42000);
 
+	printf("%s\n", str);
 	printf("%s\n", str2);
-}*/
+
+	free(str2);
+}
