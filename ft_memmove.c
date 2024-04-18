0cb0f54be8935736f4990d8b1dbabe7d866121c0
Rui Alexandre Andrade Goncalves <randrade@c1r9s1.42lisboa.com>
2024-04-18 13:01:20 +0100

    18/04 13:01

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: randrade <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:29:24 by randrade          #+#    #+#             */
-/*   Updated: 2024/04/18 01:24:38 by randrade         ###   ########.fr       */
+/*   Updated: 2024/04/18 13:00:22 by randrade         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,36 +17,38 @@ void	*ft_memmove(void *dest, const void *src, size_t len)
 	int	i;
 
 	i = len - 1;
-	if (dest == NULL && src == NULL)
-		return (NULL);
+//	if (dest == NULL && src == NULL)
+//		return (NULL);
 	if (src < dest)
 	{
-		while (
+		while (i >= 0)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i--;
+		}
 	}
-	while (i >= 0)
+	else
 	{
-		((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
-		i--;
+		i = 0;
+		while (i < len)
+		{
+			((unsigned char *)dest)[i] = ((unsigned char *)src)[i];
+			i++;
+		}
 	}
 	return (dest);
 }
-
+/*
 #include <stdio.h>
 #include <string.h>
 
 int	main(void)
 {
-	char	str1[] = "lorem ipsum dolor sit amet";
-	char	str2[] = "orem ipsum dolor sit amet";
+	char	str1[] = "Hello World!";
+	char	str2[] = "Hey, ";
 
 	printf("%s\n", str1);
-	char	*ptr1 = (char *)ft_memmove(str1, str2, 8);
+	char	*ptr1 = (char *)ft_memmove(str2, str1, 4);
 	printf("%s\n", str2);
 	printf("%s\n\n", ptr1);
-/*
-	printf("%s\n", str1);
-	char	*ptr2 = (char *)ft_memmove(str1, str2, 8);
-	printf("%s\n", str2);
-	printf("%s\n", ptr2);
-*/
-}
+}*/
